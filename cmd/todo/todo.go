c11eeb442b9f65cb8ce05dadb42bb321cd6b1b2f
AliiAhmadi <aliahmadi.github@gmail.com>
2024-02-26 14:58:51 +0330

    + create 'add' and 'complete' methods for list object

--- cmd/todo/todo.go
+++ cmd/todo/todo.go
@@ -1,14 +1,39 @@
 package todo
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // item struct represents todo items
 type item struct {
-	Task      string    `json:"task"`
-	Done      bool      `json:"done"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	Task        string    `json:"task"`
+	Done        bool      `json:"done"`
+	CreatedAt   time.Time `json:"created_at"`
+	CompletedAt time.Time `json:"updated_at"`
 }
 
 // List represents all todos in application
 type List []item
+
+// Add creates a new task and add that to list
+func (l *List) Add(task string) {
+	tk := item{
+		Task:        task,
+		Done:        false,
+		CreatedAt:   time.Now(),
+		CompletedAt: time.Time{},
+	}
+	*l = append(*l, tk)
+}
+
+// Complete makes a todo complete by change Done from false to true
+func (l *List) Complete(index int) error {
+	if index <= 0 || index > len(*l) {
+		return fmt.Errorf("item %d does not exist", index)
+	}
+
+	(*l)[index-1].Done = true
+	(*l)[index-1].CompletedAt = time.Now()
+	return nil
+}
